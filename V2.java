869d847a80e81365ccb02ad666e7d590d65334e0
HeDaGi <151563977+nath162@users.noreply.github.com>
2024-08-02 12:39:37 +0200

    Add files via upload

--- V2.java
+++ V2.java
@@ -1,1 +1,68 @@
 
+import java.util.HashMap;
+import java.util.Scanner;
+
+class V2 {
+
+    public static void gameloop() {//fonction qui appellra toute les autres fonction
+        creategrid();
+        while (dowanttoplay()) {//il faut créer c'est bool dans want to play
+            if (isblockblocked()) {//et lui c'est quand le block est blocker
+                chooseblock(createblockHashMap());
+            }
+        }
+    }
+
+    public static String[][] creategrid() {
+        String[][] grid = {
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"}};
+        return grid;
+    }
+
+    public static HashMap<Integer, String[][]> createblockHashMap() {
+        HashMap<Integer, String[][]> blocks = new HashMap<>();
+        blocks.put(0, new String[][]{{"+", "+", "+", "+"}});
+        blocks.put(1, new String[][]{{"+", "*", "*"}, {"+", "+", "+"}});
+        blocks.put(2, new String[][]{{"*", "*", "+"}, {"+", "+", "+"}});
+        blocks.put(3, new String[][]{{"+", "+"}, {"+", "+"}});
+        blocks.put(4, new String[][]{{"+", "+", "*"}, {"*", "+", "+"}});
+        blocks.put(5, new String[][]{{"*", "+", "*"}, {"+", "+", "+"}});
+        blocks.put(6, new String[][]{{"*", "+", "+"}, {"+", "+", "*"}});
+        return blocks;
+    }
+
+    public static String[][] chooseblock(HashMap<Integer, String[][]> blockshashmap) {
+        String[][] choosenblock = blockshashmap.get((int) (Math.random() * 8));
+        return choosenblock;
+    }
+
+    public static boolean dowanttoplay() {
+        Scanner usrinput = new Scanner(System.in);
+        boolean play = !usrinput.equals("n");
+        return play;
+    }
+
+    public static boolean isblockblocked() {
+        boolean blockblocked = false;
+        //faire en sorte de savoir si le block est bloquer
+        return blockblocked;
+    }
+}
