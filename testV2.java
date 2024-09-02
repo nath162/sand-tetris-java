72ea3a70f6bd8b3c59746f1f447f53cd0ca68910
HeDaGi <151563977+nath162@users.noreply.github.com>
2024-09-02 14:25:37 +0000

    pre et i sont mal placé

--- testV2.java
+++ testV2.java
@@ -1,6 +1,6 @@
 public class testV2{
     public static void main(String[] args){
-        destroylines(creategrid());
+        displaygid(destroylines(creategrid()));
     }
     public static String[][] creategrid() {//fini
         String[][] grid = {
@@ -21,27 +21,46 @@ public class testV2{
             {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
             {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
             {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
-            {"-", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"},
+            {"-", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "-"},
             {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"}};
         return grid;
     }
-    public static void destroylines(String[][] grid) {//probleme les elements en haut ne descende pas ducoup aprés que la ligne soit détruite
+    public static String[][] destroylines(String[][] grid) {//probleme les elements en haut ne descende pas ducoup aprés que la ligne soit détruite
+        int destroy = 0;
         int pre = 0;
-        boolean destroyline = true;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; i < grid[i].length; j++) {
-                if (pre != i) {
-                    pre = i;
-                    if (destroyline) {
-                        for (int l = 0; l < grid[i].length; l++) {
-                            grid[i][l] = "*";
+        for(int i=0;i < grid.length;i++){
+            for(int j = 0;j < grid[i].length;j++){
+                if(pre != i){
+                    destroy = 0;
+                }
+                else{
+                    destroy++;
+                }
+                if(grid[i][j] == "+"){
+                    if(destroy == 10){
+                            //delete la row qu'on veut pas puis ajouter une row tout en haut neuve
                         }
                     }
-                    destroyline = true;
                 }
-                if (grid[i][j].equals("*")) {
-                    destroyline = false;
+                pre = i;
+            }
+        }
+        return grid;
+    }
+    public static void displaygid(String[][] grid) {
+        int count = 0;
+        int pre = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 1; j < grid[i].length; j++) {
+                if (pre != i || count ==0) {
+                    count +=1;
+                    System.out.println(" ");
+                    System.out.print("-");
+                    System.out.print(grid[i][j]);
+                } else {
+                    System.out.print(grid[i][j]);
                 }
+                pre = i;
             }
         }
     }
